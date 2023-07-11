ad5c4f4482106e4bbe1cf49393257c5a1c877691
YandyLema <yandylema@hotmail.com>
2023-07-11 11:56:16 -0700

    added some html to AddItemPage

--- yourshare/src/pages/AddItemPage.js
+++ yourshare/src/pages/AddItemPage.js
@@ -5,7 +5,8 @@ export class AddItemPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Add item</h1>
+        <h1>Add a cool item</h1>
+        <h2>What will you add?</h2>
         <p style={{position: "fixed", top: "10px", left:"10px"}} onClick={(e) => this.props.changePage(pages.WelcomePage)}>
           Back
         </p>
