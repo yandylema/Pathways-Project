be55be2365275f029dcc4649cb814f28d05cc8e3
Hazel Pham <v-hazelpham@microsoft.com>
2023-07-11 14:44:21 -0700

    edit css file folder

--- yourshare/src/components/InputText.js
+++ yourshare/src/components/InputText.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+const inputBoxText = {
+  padding: "20px",
+  marginBottom: "40px",
+  marginTop: "40px",
+  border: "0px",
+  borderBottom: "2px solid black",
+  width: "350px",
+};
 const InputText = (props) => {
   return (
     <div>
-      <input className="inputBoxText" placeholder={props.placeholderInput} />
+      <input style={inputBoxText} placeholder={props.placeholderInput} />
     </div>
   );
 };
--- yourshare/src/pages/SignupPage.js
+++ yourshare/src/pages/SignupPage.js
@@ -2,12 +2,24 @@ import React from "react";
 import { pages } from "../App";
 import InputText from "../components/InputText";
 import logo from "../images/YS_icon.png";
+
+const signUpPage = {
+  margin: "auto",
+  backgroundColor: "white",
+  display: "flex",
+  justifyContent: "space-around",
+};
+const img = {
+  width: "450px",
+  height: "450px",
+  paddingRight: "250px",
+};
 export class SignupPage extends React.Component {
   render() {
     return (
-      <div className="signUpPage">
+      <div style={signUpPage}>
         <div className="logoSignUpPage">
-          <img src={logo}></img>
+          <img style={img} src={logo}></img>
         </div>
         <div className="signUpPageInfo">
           <h1>Join our community</h1>
