feb768a014742d10651c2aebedb892033fc076e7
YandyLema <yandylema@hotmail.com>
2023-07-11 15:32:59 -0700

    Added buttons and input boxes

--- yourshare/src/pages/AddItemPage.js
+++ yourshare/src/pages/AddItemPage.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { pages } from "../App";
+import InputText from "../components/InputText";
+import { Button } from "../components/Button";
+import { CancelButton } from "../components/CancelButton";
+import Textbox from "../components/Textbox";
+
+const h1 = {
+  paddingLeft:"150px"
+}
+const textbox = {
+  width:"500px",
+  height: "450px"
+}
 
 export class AddItemPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Add a cool item</h1>
-        <h2>What will you add?</h2>
+        <h1 style={h1}>Add an item</h1>
+        <p>Item name:</p>
+        <InputText placeholderInput = {"Item name:"}></InputText>
+        <p>Type:</p>
+        <InputText placeholderInput = {"Type:"}></InputText>
+        <p>Description</p>
+        <Textbox style={textbox}></Textbox>
         <p style={{position: "fixed", top: "10px", left:"10px"}} onClick={(e) => this.props.changePage(pages.WelcomePage)}>
           Back
         </p>
+        <CancelButton>Cancel</CancelButton>
+        <Button>Add item</Button>
       </div>
     );
   }
