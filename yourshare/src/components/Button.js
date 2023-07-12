66c935fcbf9a7a31347c59a958c50e2389c5e628
jpbonch <jp.bonchristiano@gmail.com>
2023-07-12 08:56:53 -0700

    fixed button onclick

--- yourshare/src/components/Button.js
+++ yourshare/src/components/Button.js
@@ -5,10 +5,11 @@ const button = {
   color: "white",
   padding: "10px",
   border: "0",
+  margin: "10px"
 };
 
 export class Button extends React.Component {
   render() {
-    return <button style={button}>{this.props.children}</button>;
+    return <button style={button} onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
