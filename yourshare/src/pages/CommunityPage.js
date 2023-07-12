611a3b3722c1e356421b55284285bb5435529271
lilum <lilumontoya.net@gmail.com>
2023-07-12 10:04:15 -0700

    added table to community page

--- yourshare/src/pages/CommunityPage.js
+++ yourshare/src/pages/CommunityPage.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { pages } from "../App";
 import { Button } from "../components/Button";
-import { CancelButton } from "../components/CancelButton";
 import { Checkbox } from "../components/Checkbox";
+import { Table } from "../components/Table";
+
+const startingData = [
+  ["Friends", "Items currently borrowed", "Items currently lent", "Bestfriend"],
+  ["Stacey", "None", "None", <Checkbox></Checkbox>],
+  ["Marcos", "None", "None", <Checkbox></Checkbox>],
+  ["Cam", "None", "None", <Checkbox></Checkbox>],
+  ["Jim", "None", "None", <Checkbox></Checkbox>],
+];
 
 export class CommunityPage extends React.Component {
   render() {
     return (
       <div>
         <h1>Manage Community</h1>
-        <p onClick={(e) => this.props.changePage(pages.BorrowItemPage)}>
-          Example Item
-        </p>
+        <div>
+          <Table data={startingData}></Table>
+        </div>
         <p
           style={{ position: "fixed", top: "10px", left: "10px" }}
           onClick={(e) => this.props.changePage(pages.WelcomePage)}
@@ -19,8 +27,7 @@ export class CommunityPage extends React.Component {
           Back
         </p>
         <p>
-          <Button>jhgf</Button>
-          <CancelButton changePage={this.props.changePage}>gvlg</CancelButton>
+          <Button>Add Friend</Button>
         </p>
         <Checkbox>Text me when someone wants to borrow an item</Checkbox>
         <Checkbox>Allow best friends to auto borrow without approval</Checkbox>
