5b75a4ccda3a0f1fc0a79039650586fffb5026c4
Joao Bonchristiano <jp.bonchristiano@gmail.com>
2023-07-16 23:08:43 -0700

    added routing for final project

--- pathway/App.js
+++ pathway/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState } from "react";
+import AuthRouter from "./routers/AuthRouter";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthRouter></AuthRouter>
   );
 }
