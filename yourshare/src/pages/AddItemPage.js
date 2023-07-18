ffdf974c6078e8cc457411bffae75924e5de8877
YandyLema <yandylema@hotmail.com>
2023-07-18 11:27:32 -0700

    Upload Box component

--- yourshare/src/pages/AddItemPage.js
+++ yourshare/src/pages/AddItemPage.js
@@ -4,19 +4,27 @@ import { Button } from "../components/Button";
 import { CancelButton } from "../components/CancelButton";
 import Textbox from "../components/Textbox";
 import { BackButton } from "../components/BackButton";
+import UploadBox from "../components/UploadBox"
+
 
 export class AddItemPage extends React.Component {
   render() {
     return (
       <div>
+        <div className="panel">
+        <div>
         <BackButton>Add Item</BackButton>
         <p>Item name:</p>
-        <InputText placeholderInput = {"Item name:"}></InputText>
-        <p>Type:</p>
-        <InputText placeholderInput = {"Type:"}></InputText>
+        <InputText placeholderInput = {"Item"}></InputText>
+        <p>Type name:</p>
+        <InputText placeholderInput = {"Type"}></InputText>
         <p>Description</p>
         <Textbox></Textbox>
-        
+        </div>
+         <div>
+        <UploadBox></UploadBox> 
+        </div>
+      </div>
         <CancelButton>Cancel</CancelButton>
         <Button>Add item</Button>
       </div>
