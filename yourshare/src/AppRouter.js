1d1e8db1a9f9cdef569ce5aa3477b52a20c0a5d3
BJ <v-bjkaious@microsoft.com>
2023-07-18 16:27:14 -0700

    BorrowItemUpdate

--- yourshare/src/AppRouter.js
+++ yourshare/src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { SignupPage } from "./pages/SignupPage";
 import NotFound from "./pages/NotFound";
 import { WelcomePage } from "./pages/WelcomePage";
@@ -7,21 +7,19 @@ import { AddItemPage } from "./pages/AddItemPage";
 import { CommunityPage } from "./pages/CommunityPage";
 import { BorrowItemPage } from "./pages/BorrowItemPage";
 
-
-
 const AppRouter = () => (
-    <Router>
-      <div  style={{margin: "auto", maxWidth: "620px"}}>
-        <Routes>
-          <Route path="/" end element={<SignupPage />} />
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/additem" element={<AddItemPage />} />
-          <Route path="/community" element={<CommunityPage />} />
-          <Route path="/borrowitem" element={<BorrowItemPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-  
-  export default AppRouter;
+  <Router>
+    <div style={{ margin: "auto", maxWidth: "620px" }}>
+      <Routes>
+        <Route path="/" end element={<SignupPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/additem" element={<AddItemPage />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/borrowitem" element={<BorrowItemPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  </Router>
+);
+
+export default AppRouter;
