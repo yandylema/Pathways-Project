cc39438fbd1e6a17a412dd902c9203467c8f56e9
Joao Pedro Amaral Bonchristiano (Cyborg Mobile LLC) <v-joaopedroa@microsoft.com>
2023-07-24 08:36:40 -0700

    Added Stuff :)

--- pathway/App.js
+++ pathway/App.js
@@ -4,10 +4,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useState } from "react";
 import AuthRouter from "./routers/AuthRouter";
-
+import {RecoilRoot} from "recoil";
 
 export default function App() {
   return (
+    <RecoilRoot>
     <AuthRouter></AuthRouter>
+    </RecoilRoot>
   );
 }
--- pathway/routers/LegalRouter.js
+++ pathway/routers/LegalRouter.js
@@ -1,5 +1,22 @@
 import Legal from "../pages/legal/Legal"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Generate from "../pages/generate/Generate";
+import Logo from "../pages/generate/Logo";
+import SocialMedia from "../pages/generate/SocialMedia";
+import SettingsButton from "../components/SettingsButton";
+import ToDo from "../pages/todo/ToDo";
+
+const Stack = createNativeStackNavigator();
+
 
 export default function LegalRouter() {
-    return <Legal></Legal>
+    return (
+        <Stack.Navigator>
+          <Stack.Screen name="Legal" component={Legal} options={({navigation}) => ({
+                headerTitle: "Pathway",
+                headerLeft: null,
+                headerRight: () => <SettingsButton navigation={navigation} />
+              })}/>
+        </Stack.Navigator>
+    );
 }
