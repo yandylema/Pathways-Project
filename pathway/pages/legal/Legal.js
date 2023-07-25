46b85690b8bf2bc420b7eb0d5f70515c8a19ad2b
lilum <lilumontoya.net@gmail.com>
2023-07-24 18:37:26 -0700

    info button with i

--- pathway/pages/legal/Legal.js
+++ pathway/pages/legal/Legal.js
@@ -1,6 +1,18 @@
 import { Text, Button, View } from "react-native";
-
+import WhiteButton from "../../components/WhiteButton";
+import InfoButton from "../../components/InfoButton";
+import ButtonWithI from "../../components/ButtonWithI";
 
 export default function Legal({ navigation }) {
-    return <Text>legal page</Text>
+  return (
+    <View>
+      <Text>legal page</Text>
+      <ButtonWithI
+        onPress={() => {
+          // navi
+        }}
+        text="Business Document"
+      />
+    </View>
+  );
 }
