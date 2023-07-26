20ae121d30429e9a8eeaaf88df57d70427b48dc5
lilum <lilumontoya.net@gmail.com>
2023-07-26 11:49:25 -0700

    new checklist component, todo list, and generate social media page started

--- pathway/pages/generate/SocialMedia.js
+++ pathway/pages/generate/SocialMedia.js
@@ -1,6 +1,26 @@
-import { Text, Button, View } from "react-native";
+import { Text, Button, View, Image } from "react-native";
+import PageTitle from "../../components/PageTitle";
 
-
+const upload = require("../../assets/upload.png");
 export default function SocialMedia({ navigation }) {
-    return <Text>social media page</Text>
+  return (
+    <View>
+      <PageTitle>Social Media Marketing</PageTitle>
+      <Text
+        style={{
+          alignSelf: "center",
+          fontFamily: "Jost",
+          fontWeight: "bold",
+          fontSize: 20,
+        }}
+      >
+        Insert Image
+      </Text>
+      <Image
+        source={upload}
+        style={{ width: 400, height: 400, alignSelf: "center" }}
+      />
+      
+    </View>
+  );
 }
--- pathway/pages/todo/ToDo.js
+++ pathway/pages/todo/ToDo.js
@@ -1,35 +1,62 @@
 import { StyleSheet, Text, Button, View } from "react-native";
 import CheckList from "../../components/CheckList";
 import { useState } from "react";
-import Tasks from "../../components/Tasks";
 import PageTitle from "../../components/PageTitle";
+import Dropdown from "../../components/DropdownChecklist";
 
-export default function ToDo({ navigation }) {
-  const [click, setClick] = useState(true);
+
+const ToDo = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = ["Option 1", "Option 2", "Option 3", "Option 4"];
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <View>
       <PageTitle>To-Do List</PageTitle>
-      <View>
-        <CheckList name={"Legal Docs"} percentage={"75%"}></CheckList>
-        <Tasks styleprop={click}> 1. Submit Business License</Tasks>
-        <CheckList name={"Marketing"} percentage={"15%"}></CheckList>
-        <CheckList name={"Geographic"} percentage={"50%"}></CheckList>
+      <View style={styles.container}>
+        <Dropdown
+          label="Legal Documents"
+          options={options}
+          onSelect={handleOptionSelect}
+        />
+
+        <Dropdown
+          label="Location"
+          options={options}
+          onSelect={handleOptionSelect}
+        />
+
+        <Dropdown
+          label="Marketing"
+          options={options}
+          onSelect={handleOptionSelect}
+        />
+        <Dropdown
+          label="Demographics"
+          options={options}
+          onSelect={handleOptionSelect}
+        />
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     width: "95%",
-    height: 200,
     borderRadius: 3,
     backgroundColor: "E9E4E4",
     display: "flex",
+    alignSelf: "center",
+    flex: 1,
   },
-  text: {
-    fontSize: 50,
-    textAlign: "center",
-    marginBottom: 8,
+  selectedOptionContainer: {
+    marginTop: 20,
   },
 });
+
+export default ToDo;
