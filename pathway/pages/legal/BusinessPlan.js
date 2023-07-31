9601697c33975fa3d9edffd84aff61ee69bea9c2
YandyLema <yandylema@hotmail.com>
2023-07-31 13:03:05 -0700

    Finished business plan page

--- pathway/pages/legal/BusinessPlan.js
+++ pathway/pages/legal/BusinessPlan.js
@@ -8,6 +8,26 @@ export default function BusinessPlan() {
       {/* Other sections of the business plan */}
       <PageTitle>PhoExpress Plan</PageTitle>
       <View style={styles.section}>
+        
+        <View style={styles.subSection}>
+          <Text style={styles.content}>
+            Objective: Serve authentic, quick Vietnamese Pho to Seattle residents and visitors.
+          </Text>
+        </View>
+
+        <View style={styles.subSection}>
+          <Text style={styles.content}>
+            Location: Central Seattle (near offices/universities).
+          </Text>
+        </View>
+
+        <View style={styles.subSection}>
+          <Text style={styles.content}>
+            Opening: November 1st, 2023.
+          </Text>
+        </View>
+
+        
         <Text style={styles.title}>Products:</Text>
         <View style={styles.subSection}>
           <Text style={styles.subTitle}>Main:</Text>
@@ -25,6 +45,7 @@ export default function BusinessPlan() {
           <Text style={styles.subTitle}>Specials:</Text>
           <Text style={styles.content}>Seasonal pho & promotions.</Text>
         </View>
+        
         <Text style={styles.title}>Market:</Text>
         <View style={styles.subSection}>
           <Text style={styles.subTitle}>Target:</Text>
@@ -44,6 +65,98 @@ export default function BusinessPlan() {
             Quick service + consistent quality.
           </Text>
         </View>
+        
+        <Text style={styles.title}>Marketing:</Text>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Launch:</Text>
+          <Text style={styles.content}>
+            Grand opening promotions.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Partners:</Text>
+          <Text style={styles.content}>
+            Local business collaboration.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Social:</Text>
+          <Text style={styles.content}>Instagram, Facebook, and Twitter.</Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Loyalty:</Text>
+          <Text style={styles.content}>Punch card or app rewards.</Text>
+        </View>
+
+        
+        <Text style={styles.title}>Operations:</Text>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Hours:</Text>
+          <Text style={styles.content}>
+            11 AM - 10 PM (extended weekends).
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Suppliers:</Text>
+          <Text style={styles.content}>
+            Local farmers & distributors.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Staff:</Text>
+          <Text style={styles.content}>Experienced chefs & trained customer service team.</Text>
+        </View>
+        
+        <Text style={styles.title}>Finances:</Text>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Startup:</Text>
+          <Text style={styles.content}>
+            Rent, decor, equipment, inventory, licenses, marketing.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Ongoing:</Text>
+          <Text style={styles.content}>
+            Rent, utilities, salaries, ingredients.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Goal:</Text>
+          <Text style={styles.content}>Calculate meals/month for break-even.</Text>
+        </View>
+        
+        <Text style={styles.title}>Challenges & Solutions:</Text>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Saturation:</Text>
+          <Text style={styles.content}>
+            Update menu based on feedback.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Seasons:</Text>
+          <Text style={styles.content}>
+            Offer delivery in winter.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Supplies:</Text>
+          <Text style={styles.content}>Multiple suppliers for consistency.</Text>
+        </View>
+
+        <Text style={styles.title}>Future:</Text>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Year 1:</Text>
+          <Text style={styles.content}>
+            Brand building & refining ops.
+          </Text>
+        </View>
+        <View style={styles.subSection}>
+          <Text style={styles.subTitle}>Year 3-5:</Text>
+          <Text style={styles.content}>
+            Expansion & franchising.
+          </Text>
+        </View>
+
       </View>
       {/* Continue with other sections of the business plan */}
     </ScrollView>
@@ -53,6 +166,7 @@ export default function BusinessPlan() {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+    textAlign: "center"
   },
   section: {
     marginBottom: 20,
@@ -67,7 +181,7 @@ const styles = StyleSheet.create({
   },
   subTitle: {
     fontSize: 16,
-    fontWeight: "bold",
+    // fontWeight: "bold",
   },
   content: {
     fontSize: 16,
