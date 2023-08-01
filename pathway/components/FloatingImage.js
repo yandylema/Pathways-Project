3c8c4cf464bb2432010ea545ee88ef132a77798c
lilum <lilumontoya.net@gmail.com>
2023-07-31 17:32:13 -0700

    fixed floating image

--- pathway/components/FloatingImage.js
+++ pathway/components/FloatingImage.js
@@ -12,13 +12,13 @@ const FloatingImage = () => {
     Animated.loop(
       Animated.sequence([
         Animated.timing(positionValue, {
-          toValue: 15,
+          toValue: 1,
           duration: 2000,
           easing: Easing.linear,
           useNativeDriver: true,
         }),
         Animated.timing(positionValue, {
-          toValue: -15,
+          toValue: 0,
           duration: 2000,
           easing: Easing.linear,
           useNativeDriver: true,
@@ -27,12 +27,17 @@ const FloatingImage = () => {
     ).start();
   };
 
+  const floatInterpolate = positionValue.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [30, 0, -30], // Adjust this value for the desired floating height
+  });
+
   return (
     <Animated.Image
-      source={require("../assets/splashimage2.png")}
+      source={require("../assets/splashimage3.png")}
       style={[
         styles.floatingImage,
-        { transform: [{ translateY: positionValue }] },
+        { transform: [{ translateY: floatInterpolate }] },
       ]}
     />
   );
@@ -55,8 +60,71 @@ const styles = StyleSheet.create({
     fontWeight: "300",
   },
   floatingImage: {
-    width: 230,
+    width: 250,
     height: 300,
     marginBottom: 20,
   },
 });
+
+// import React, { useState, useEffect } from "react";
+// import { Animated, Easing, StyleSheet } from "react-native";
+
+// const FloatingImage = () => {
+//   const [positionValue] = useState(new Animated.Value(0));
+
+//   useEffect(() => {
+//     startFloating();
+//   }, []);
+
+//   const startFloating = () => {
+//     Animated.loop(
+//       Animated.sequence([
+//         Animated.timing(positionValue, {
+//           toValue: 15,
+//           duration: 2000,
+//           easing: Easing.linear,
+//           useNativeDriver: true,
+//         }),
+//         Animated.timing(positionValue, {
+//           toValue: -15,
+//           duration: 2000,
+//           easing: Easing.linear,
+//           useNativeDriver: true,
+//         }),
+//       ])
+//     ).start();
+//   };
+
+//   return (
+//     <Animated.Image
+//       source={require("../assets/splashimage3.png")}
+//       style={[
+//         styles.floatingImage,
+//         { transform: [{ translateY: positionValue }] },
+//       ]}
+//     />
+//   );
+// };
+// export default FloatingImage;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "space-between",
+//     paddingBottom: 20,
+//     paddingTop: 30,
+//     alignItems: "center",
+//   },
+//   caption: {
+//     textAlign: "center",
+//     color: "white",
+//     fontFamily: "Jost",
+//     fontSize: 20,
+//     fontWeight: "300",
+//   },
+//   floatingImage: {
+//     width: 250,
+//     height: 300,
+//     marginBottom: 20,
+//   },
+// });
